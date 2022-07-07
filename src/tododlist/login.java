a607072fc0359130d4193fe3f4b4408ae676efae
unknown <aneeshsharmachd@gmail.com>
2022-07-06 21:07:51 -0400

    Login page created to authenticate existing users in the DB

--- src/tododlist/login.java
+++ src/tododlist/login.java
@@ -1,15 +1,31 @@
 package tododlist;
 
+
+import java.sql.ResultSet;
+import javax.swing.JOptionPane;
 import java.awt.BorderLayout;
+
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.awt.event.ActionEvent;
 
 public class login extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JPasswordField passwordField;
 
 	/**
 	 * Launch the application.
@@ -30,13 +46,83 @@ public class login extends JFrame {
 	/**
 	 * Create the frame.
 	 */
+	
+	Connection con = null;
+	
 	public login() {
+		
+		con = (Connection)DB.dbconnect();
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 457, 307);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Login Form");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblNewLabel.setBounds(10, 0, 367, 75);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("User ID");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setBounds(10, 85, 133, 26);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Password");
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblNewLabel_2.setBounds(10, 134, 133, 26);
+		contentPane.add(lblNewLabel_2);
+		
+		textField = new JTextField();
+		textField.setBounds(211, 91, 166, 19);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(211, 140, 166, 19);
+		contentPane.add(passwordField);
+		
+		JButton btnNewButton = new JButton("Login");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				try
+				{
+					String user = textField.getText();
+					String pwd = String.valueOf(passwordField.getPassword());
+					
+					PreparedStatement pst = (PreparedStatement) con.prepareStatement("select * from signup where userid=? and password=?");
+					
+					pst.setString(1,user);
+					pst.setString(2,pwd);
+					
+					ResultSet r = pst.executeQuery();
+					
+					if(r.next())
+					{
+						JOptionPane.showMessageDialog(null, "Welcome!");
+					}
+					else
+					{
+						JOptionPane.showMessageDialog(null, "incorrect id or password");
+					}
+					
+					
+				}
+				catch (Exception e1)
+				{
+					e1.printStackTrace();
+				}
+				
+			}
+		});
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		btnNewButton.setBounds(147, 200, 121, 38);
+		contentPane.add(btnNewButton);
 	}
-
 }
