c37da999acc9c7d5ac81867a5a5706a6a7b1b7a5
unknown <aneeshsharmachd@gmail.com>
2022-07-06 20:52:01 -0400

    Database class for connection implemented

--- src/tododlist/DB.java
+++ src/tododlist/DB.java
@@ -1,5 +1,29 @@
 package tododlist;
 
-public class DB {
+import javax.swing.*;
+//import com.mysql.jdbc.ResultSetMetaData;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DB 
+{
+	Connection con = null;
+	java.sql.PreparedStatement pst;
+	
+	public static Connection dbconnect()
+	{
+		try
+		{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:4306/todolist","root","");
+			return conn;
+		}
+		catch(Exception e2)
+		{
+			System.out.println(e2);
+			return null;
+		}
+	}
 
 }
